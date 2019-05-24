ef7ce57a8c28fdbf64fd77a6c80d6d76cafd5e37
ketanchoyal <sharmaketanr@gmail.com>
2019-05-25 01:32:24 +0530

    Refractoring

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'reusable_card.dart';
-import 'reusable_card_child.dart';
-import 'constants.dart';
-import 'result_page.dart';
-import 'round_icon_button.dart';
-import 'bottom_button.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/components/reusable_card_child.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/screens/result_page.dart';
+import 'package:bmi_calculator/components/round_icon_button.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
 
 enum Gender { Male, Female }
 
